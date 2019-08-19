65c82982d45e2586380e51b3fd56bfe4012ea479
kelveenfabian <kelveenfabian@pursuit.org>
2019-08-19 09:59:06 -0400

    Create LocationDao interface and LocationDatabase abstract class for Room setup

--- app/src/main/java/com/goTenna/codingchallenge/data/database/LocationDao.java
+++ app/src/main/java/com/goTenna/codingchallenge/data/database/LocationDao.java
@@ -1,7 +1,6 @@
 package com.goTenna.codingchallenge.data.database;
 
 
-import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
@@ -11,10 +10,7 @@ import com.goTenna.codingchallenge.data.model.Location;
 
 import java.util.List;
 
-import io.reactivex.Completable;
 import io.reactivex.Flowable;
-import io.reactivex.Observable;
-import io.reactivex.Single;
 
 @Dao
 public interface LocationDao {
@@ -24,9 +20,6 @@ public interface LocationDao {
 
     @Delete
     void delete(Location location);
-
-    @Query("DELETE FROM location_table")
-    void deleteAllLocations();
 
     @Query("SELECT * FROM location_table ORDER BY id ASC")
     Flowable<List<Location>> getAllLocations();
--- app/src/main/java/com/goTenna/codingchallenge/data/database/LocationDatabase.java
+++ app/src/main/java/com/goTenna/codingchallenge/data/database/LocationDatabase.java
@@ -1,74 +1,26 @@
 package com.goTenna.codingchallenge.data.database;
 
-import android.annotation.SuppressLint;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.util.Log;
 
 import com.goTenna.codingchallenge.data.model.Location;
-import com.goTenna.codingchallenge.data.repository.LocationRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.Completable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-
-@Database(entities = {Location.class}, version = 1, exportSchema = false)
+@Database(entities = {Location.class}, version = 1)
 public abstract class LocationDatabase extends RoomDatabase {
     private static LocationDatabase instance;
+    private static final String DB_NAME = "location_db";
 
     public abstract LocationDao locationDao();
 
     public static synchronized LocationDatabase getInstance(Context context){
         if(instance == null){
-            instance = Room.databaseBuilder(context.getApplicationContext(), LocationDatabase.class,"location_database")
+            instance = Room.databaseBuilder(context.getApplicationContext(), LocationDatabase.class, DB_NAME)
                     .fallbackToDestructiveMigration()
                     .build();
         }
 
         return instance;
-    }
-
-    @SuppressLint("CheckResult")
-    public List<Location> getAllLocations(){
-        final List<Location> locationList = new ArrayList<>();
-
-        locationDao().getAllLocations()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<Location>>() {
-                    @Override
-                    public void accept(List<Location> locations) throws Exception {
-                        locationList.addAll(locations);
-                    }
-                });
-
-        return locationList;
-    }
-
-    @SuppressLint("CheckResult")
-    public void insertLocation(final Location location){
-        Completable.fromAction(() -> locationDao().insert(location));
-        Log.d(LocationRepository.TAG, location.getName() + " is inside of database");
-    }
-
-    @SuppressLint("CheckResult")
-    public void deleteLocation(final Location location){
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() throws Exception {
-                locationDao().delete(location);
-            }
-        });
-    }
-
-    public void deleteAllLocations(){
-        locationDao().deleteAllLocations();
     }
  }
