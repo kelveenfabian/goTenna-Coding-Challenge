64c2b2372d32f3e7ff41d9e5be8e94b7a0d59204
kelveenfabian <kelveenfabian@pursuit.org>
2019-08-19 09:43:41 -0400

    Add database instance to communicate with insert, delete, and deleteAll queries

--- app/src/main/java/com/goTenna/codingchallenge/data/repository/LocationRepository.java
+++ app/src/main/java/com/goTenna/codingchallenge/data/repository/LocationRepository.java
@@ -2,70 +2,134 @@ package com.goTenna.codingchallenge.data.repository;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
-import android.util.Log;
-import android.widget.Toast;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.LiveDataReactiveStreams;
 
-import com.goTenna.codingchallenge.MainActivity;
 import com.goTenna.codingchallenge.data.database.LocationDatabase;
 import com.goTenna.codingchallenge.data.model.Location;
 import com.goTenna.codingchallenge.network.LocationRetroFit;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Completable;
 import io.reactivex.CompletableObserver;
 import io.reactivex.Flowable;
-import io.reactivex.FlowableSubscriber;
-import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
+
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class LocationRepository {
     private LocationDatabase locationDatabase;
     public static final String TAG = "TAG";
+    private static LocationRepository instance;
+    private CompositeDisposable disposables = new CompositeDisposable();
 
-    public LocationRepository(Application application){
+    private LocationRepository(Application application){
         locationDatabase = LocationDatabase.getInstance(application.getApplicationContext());
     }
 
-
-    public List<Location> getAllLocations(){
-        return locationDatabase.getAllLocations();
+    public static LocationRepository getInstance(Application application){
+        if(instance == null){
+            instance = new LocationRepository(application);
+        }
+        return instance;
     }
 
+    @SuppressLint("CheckResult")
+    public Flowable<List<Location>> getAllLocations(){
+        return locationDatabase.locationDao().getAllLocations();
+    }
+
+    @SuppressLint("CheckResult")
     public void insertLocation(final Location location){
-        locationDatabase.insertLocation(location);
-    }
+//        Completable.fromAction(() -> locationDatabase.locationDao().insert(location));
 
-    public void deleteAllLocations(){
-        locationDatabase.deleteAllLocations();
+        Completable.fromRunnable(() -> locationDatabase.locationDao().insert(location))
+                .subscribeOn(Schedulers.io())
+        .subscribe(new CompletableObserver() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposables.add(d);
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+        });
+
+//        locationDatabase.locationDao().insert(location);
     }
 
     @SuppressLint("CheckResult")
     public void deleteLocation(final Location location){
-        locationDatabase.locationDao().delete(location);
-    }
+//        Completable.fromAction(() -> locationDatabase.locationDao().delete(location));
 
+        Completable.fromRunnable(() -> locationDatabase.locationDao().delete(location))
+                .subscribeOn(Schedulers.io())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposables.add(d);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+
+//        locationDatabase.locationDao().delete(location);
+    }
 
     @SuppressLint("CheckResult")
-    public void callRetroFit(){
-        LocationRetroFit.getService()
-                .getLocation()
+    public void deleteAllLocations(){
+        Completable.fromAction(() -> locationDatabase.clearAllTables());
+
+        Completable.fromRunnable(() -> locationDatabase.clearAllTables())
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(locations -> {
-                    for(int i = 0; i < locations.size(); i++){
-                        Log.d(TAG, "Location " + locations.get(i).getName());
-                        insertLocation(locations.get(i));
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposables.add(d);
                     }
-                    Log.d(TAG, "RetroFit: LocationSize: " + locations.size());
-                }, Throwable::printStackTrace);
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+
+//        locationDatabase.clearAllTables();
     }
 
 
+    public LiveData<List<Location>> getLocationsFromApi(){
+        return LiveDataReactiveStreams.fromPublisher(LocationRetroFit
+                .getService()
+                .getLocation()
+                .observeOn(Schedulers.io()));
+    }
+
+    public CompositeDisposable getDisposables() {
+        return disposables;
+    }
+
 }
+
